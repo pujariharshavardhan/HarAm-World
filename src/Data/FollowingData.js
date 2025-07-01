a72c91114a3e90a34e46c5c7bd06e0271805977c
pujariharshavardhan <111888165+pujariharshavardhan@users.noreply.github.com>
2025-07-01 14:22:08 +0530

    Add files via upload

---
+++ src/Data/FollowingData.js
@@ -0,0 +1,33 @@
+import Fimg1 from "../assets/Following/img-1.jpg"
+import Fimg2 from "../assets/Following/img-2.jpg"
+import Fimg3 from "../assets/Following/img-3.jpg"
+import Fimg4 from "../assets/Following/img-4.jpg"
+import Fimg5 from "../assets/Following/img-5.jpg"
+
+export const FollowimgData =[
+    {   
+        name:"What_subbu",
+        username:"@subbu",
+        img:Fimg1
+    },
+    {  
+        name:"Nani kumar",
+        username:"@Nanikumar",
+        img:Fimg2
+    },
+    { 
+        name:"Pushpa_raj",
+        username:"@pushparaj",
+        img:Fimg3
+    },
+    {   
+        name:"Lilly",
+        username:"@LillyPrincess",
+        img:Fimg4
+    },
+    {  
+        name:"Chintu",
+        username:"@Chintu123",
+        img:Fimg5
+    }
+]
